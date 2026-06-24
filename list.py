d22a0ff100ee08d4200bcca07c4805e3752e0d12
himunit24 <singhhimanshu2302@gmail.com>
2026-06-24 12:14:25 +0530

    list updated

--- list.py
+++ list.py
@@ -1,3 +1,7 @@
 l1=[40,20,30,10,50]
 print(l1[2])
 print(l1[1],l1[-4],l1[-1]) #same hai isme negative indexing ka concept hota hai aur last se shuru hota hai -1 se shuru hota hai
+print("____________________________________________________________________________________________")
+l2=[10,50,30,40,20]
+for i in l2 :
+    print(i,end=" ")
