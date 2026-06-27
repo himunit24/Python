752cc74b7288a8e06c7fd67dc107d668725060d3
himunit24 <singhhimanshu2302@gmail.com>
2026-06-27 10:20:54 +0530

    modified

--- list.py
+++ list.py
@@ -22,3 +22,32 @@ i=len(l2)-1
 while i>=0 :
     print(l2[i],end=" ")
     i=i-1
+
+print()
+l5=list(range(4))
+l6=list(range(5))
+print(l5)
+print(l6>l5)
+print(l6+l5)
+l7=l6*2
+print(l7)
+
+l1.append(10)
+l1.insert(2,100)
+print(l1)
+l1.remove(10) # if two values are present then 1 occuring value will be deleted
+print(l1)
+x=l1.pop() #it deleted last element and gives its value therefore it is stored
+print(x)
+print(l1)
+l7.clear() #it removes all element ina list
+print(l7)
+l1.reverse()
+print(l1)
+print(l1.index(50))
+
+#list comprehension
+n=int(input("Enter a number : "))
+l10=[a*2-1 for a in range(1,n+1)] 
+l9=[a*2 for a in range(1,int(input("Enter nth term : "))+1)]
+print(l10,l9)
---
+++ reversinglist.py
@@ -0,0 +1,7 @@
+l1=[]
+ch=input("Do you want to enter input in list : ")
+while ch!="No":
+    n=int(input("Enter number :"))
+    l1.append(n)
+    ch=input("Do you want to enter input in list : ")
+print(l1)
